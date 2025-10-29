6c9a801134fdfd31fccfa4fde3054d56520ef4e9
MOHITJOD <mohitfpp@gmail.com>
2025-10-29 17:40:06 +0530

    unused imports removed

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import { useUser } from "../context/UserContext";
 
 const Menu = () => {
